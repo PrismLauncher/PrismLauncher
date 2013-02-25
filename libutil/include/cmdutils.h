b56b819c35843db0d135a5bbbee5fc9916bd06df
Andrew <forkk@forkk.net>
2013-02-25 13:39:07 -0600

    Fixed some formatting.

--- libutil/include/cmdutils.h
+++ libutil/include/cmdutils.h
@@ -38,13 +38,13 @@ namespace Commandline {
  * Specifies how flags are decorated
  */
 enum class FlagStyle {
-    GNU,     /**< --option and -o (GNU Style) */
-    Unix,    /**< -option and -o  (Unix Style) */
-    Windows, /**< /option and /o  (Windows Style) */
+	GNU,     /**< --option and -o (GNU Style) */
+	Unix,    /**< -option and -o  (Unix Style) */
+	Windows, /**< /option and /o  (Windows Style) */
 #ifdef Q_OS_WIN32
-    Default = Windows
+	Default = Windows
 #else
-    Default = GNU
+	Default = GNU
 #endif
 };
 
@@ -52,13 +52,13 @@ enum class FlagStyle {
  * @brief The ArgumentStyle enum
  */
 enum class ArgumentStyle {
-    Space,          /**< --option=value */
-    Equals,         /**< --option value */
-    SpaceAndEquals, /**< --option[= ]value */
+	Space,          /**< --option=value */
+	Equals,         /**< --option value */
+	SpaceAndEquals, /**< --option[= ]value */
 #ifdef Q_OS_WIN32
-    Default = Equals
+	Default = Equals
 #else
-    Default = SpaceAndEquals
+	Default = SpaceAndEquals
 #endif
 };
 
@@ -68,13 +68,13 @@ enum class ArgumentStyle {
 class ParsingError : public std::exception
 {
 public:
-    ParsingError(const QString &what);
-    ParsingError(const ParsingError &e);
-    ~ParsingError() throw() {}
-    const char *what() const throw();
-    QString qwhat() const;
+	ParsingError(const QString &what);
+	ParsingError(const ParsingError &e);
+	~ParsingError() throw() {}
+	const char *what() const throw();
+	QString qwhat() const;
 private:
-    QString m_what;
+	QString m_what;
 };
 
 /**
@@ -83,154 +83,154 @@ private:
 class Parser
 {
 public:
-    /**
-     * @brief Parser constructor
-     * @param flagStyle the FlagStyle to use in this Parser
-     * @param argStyle the ArgumentStyle to use in this Parser
-     */
-    Parser(FlagStyle flagStyle=FlagStyle::Default, ArgumentStyle argStyle=ArgumentStyle::Default);
-
-    /**
-     * @brief set the flag style
-     * @param style
-     */
-    void setFlagStyle(FlagStyle style);
-
-    /**
-     * @brief get the flag style
-     * @return
-     */
-    FlagStyle flagStyle();
-
-    /**
-     * @brief set the argument style
-     * @param style
-     */
-    void setArgumentStyle(ArgumentStyle style);
-
-    /**
-     * @brief get the argument style
-     * @return
-     */
-    ArgumentStyle argumentStyle();
-
-    /**
-     * @brief define a boolean switch
-     * @param name the parameter name
-     * @param def the default value
-     */
-    void addSwitch(QString name, bool def=false);
-
-    /**
-     * @brief define an option that takes an additional argument
-     * @param name the parameter name
-     * @param def the default value
-     */
-    void addOption(QString name, QVariant def=QVariant());
-
-    /**
-     * @brief define a positional argument
-     * @param name the parameter name
-     * @param required wether this argument is required
-     * @param def the default value
-     */
-    void addArgument(QString name, bool required=true, QVariant def=QVariant());
-
-    /**
-     * @brief adds a flag to an existing parameter
-     * @param name the (existing) parameter name
-     * @param flag the flag character
-     * @see addSwitch addArgument addOption
-     * Note: any one parameter can only have one flag
-     */
-    void addShortOpt(QString name, QChar flag);
-
-    /**
-     * @brief adds documentation to a Parameter
-     * @param name the parameter name
-     * @param metavar a string to be displayed as placeholder for the value
-     * @param doc a QString containing the documentation
-     * Note: on positional arguments, metavar replaces the name as displayed.
-     *       on options , metavar replaces the value placeholder
-     */
-    void addDocumentation(QString name, QString doc, QString metavar=QString());
-
-    /**
-     * @brief generate a help message
-     * @param progName the program name to use in the help message
-     * @param helpIndent how much the parameter documentation should be indented
-     * @param flagsInUsage whether we should use flags instead of options in the usage
-     * @return a help message
-     */
-    QString compileHelp(QString progName, int helpIndent=22, bool flagsInUsage=true);
-
-    /**
-     * @brief generate a short usage message
-     * @param progName the program name to use in the usage message
-     * @param useFlags whether we should use flags instead of options
-     * @return a usage message
-     */
-    QString compileUsage(QString progName, bool useFlags=true);
-
-    /**
-     * @brief parse
-     * @param argv a QStringList containing the program ARGV
-     * @return a QHash mapping argument names to their values
-     */
-    QHash<QString, QVariant> parse(QStringList argv);
-
-    /**
-     * @brief clear all definitions
-     */
-    void clear();
-
-    ~Parser();
-
+	/**
+	 * @brief Parser constructor
+	 * @param flagStyle the FlagStyle to use in this Parser
+	 * @param argStyle the ArgumentStyle to use in this Parser
+	 */
+	Parser(FlagStyle flagStyle=FlagStyle::Default, ArgumentStyle argStyle=ArgumentStyle::Default);
+	
+	/**
+	 * @brief set the flag style
+	 * @param style
+	 */
+	void setFlagStyle(FlagStyle style);
+	
+	/**
+	 * @brief get the flag style
+	 * @return
+	 */
+	FlagStyle flagStyle();
+	
+	/**
+	 * @brief set the argument style
+	 * @param style
+	 */
+	void setArgumentStyle(ArgumentStyle style);
+	
+	/**
+	 * @brief get the argument style
+	 * @return
+	 */
+	ArgumentStyle argumentStyle();
+	
+	/**
+	 * @brief define a boolean switch
+	 * @param name the parameter name
+	 * @param def the default value
+	 */
+	void addSwitch(QString name, bool def=false);
+	
+	/**
+	 * @brief define an option that takes an additional argument
+	 * @param name the parameter name
+	 * @param def the default value
+	 */
+	void addOption(QString name, QVariant def=QVariant());
+	
+	/**
+	 * @brief define a positional argument
+	 * @param name the parameter name
+	 * @param required wether this argument is required
+	 * @param def the default value
+	 */
+	void addArgument(QString name, bool required=true, QVariant def=QVariant());
+	
+	/**
+	 * @brief adds a flag to an existing parameter
+	 * @param name the (existing) parameter name
+	 * @param flag the flag character
+	 * @see addSwitch addArgument addOption
+	 * Note: any one parameter can only have one flag
+	 */
+	void addShortOpt(QString name, QChar flag);
+	
+	/**
+	 * @brief adds documentation to a Parameter
+	 * @param name the parameter name
+	 * @param metavar a string to be displayed as placeholder for the value
+	 * @param doc a QString containing the documentation
+	 * Note: on positional arguments, metavar replaces the name as displayed.
+	 *       on options , metavar replaces the value placeholder
+	 */
+	void addDocumentation(QString name, QString doc, QString metavar=QString());
+	
+	/**
+	 * @brief generate a help message
+	 * @param progName the program name to use in the help message
+	 * @param helpIndent how much the parameter documentation should be indented
+	 * @param flagsInUsage whether we should use flags instead of options in the usage
+	 * @return a help message
+	 */
+	QString compileHelp(QString progName, int helpIndent=22, bool flagsInUsage=true);
+	
+	/**
+	 * @brief generate a short usage message
+	 * @param progName the program name to use in the usage message
+	 * @param useFlags whether we should use flags instead of options
+	 * @return a usage message
+	 */
+	QString compileUsage(QString progName, bool useFlags=true);
+	
+	/**
+	 * @brief parse
+	 * @param argv a QStringList containing the program ARGV
+	 * @return a QHash mapping argument names to their values
+	 */
+	QHash<QString, QVariant> parse(QStringList argv);
+	
+	/**
+	 * @brief clear all definitions
+	 */
+	void clear();
+	
+	~Parser();
+	
 private:
-    FlagStyle m_flagStyle;
-    ArgumentStyle m_argStyle;
-
-    enum class OptionType {
-        Switch,
-        Option
-    };
-
-    // Important: the common part MUST BE COMMON ON ALL THREE structs
-    struct CommonDef {
-        QString name;
-        QString doc;
-        QString metavar;
-        QVariant def;
-    };
-
-    struct OptionDef {
-        // common
-        QString name;
-        QString doc;
-        QString metavar;
-        QVariant def;
-        // option
-        OptionType type;
-        QChar flag;
-    };
-
-    struct PositionalDef {
-        // common
-        QString name;
-        QString doc;
-        QString metavar;
-        QVariant def;
-        // positional
-        bool required;
-    };
-
-    QHash<QString, OptionDef *> m_options;
-    QHash<QChar, OptionDef *> m_flags;
-    QHash<QString, CommonDef *> m_params;
-    QList<PositionalDef *> m_positionals;
-    QList<OptionDef *> m_optionList;
-
-    void getPrefix(QString &opt, QString &flag);
+	FlagStyle m_flagStyle;
+	ArgumentStyle m_argStyle;
+	
+	enum class OptionType {
+		Switch,
+		Option
+	};
+	
+	// Important: the common part MUST BE COMMON ON ALL THREE structs
+	struct CommonDef {
+		QString name;
+		QString doc;
+		QString metavar;
+		QVariant def;
+	};
+	
+	struct OptionDef {
+		// common
+		QString name;
+		QString doc;
+		QString metavar;
+		QVariant def;
+		// option
+		OptionType type;
+		QChar flag;
+	};
+	
+	struct PositionalDef {
+		// common
+		QString name;
+		QString doc;
+		QString metavar;
+		QVariant def;
+		// positional
+		bool required;
+	};
+	
+	QHash<QString, OptionDef *> m_options;
+	QHash<QChar, OptionDef *> m_flags;
+	QHash<QString, CommonDef *> m_params;
+	QList<PositionalDef *> m_positionals;
+	QList<OptionDef *> m_optionList;
+	
+	void getPrefix(QString &opt, QString &flag);
 };
 
 }
--- libutil/include/userutils.h
+++ libutil/include/userutils.h
@@ -5,13 +5,13 @@
 
 namespace Util
 {
-    // Get the Directory representing the User's Desktop
-    QString getDesktopDir();
+// Get the Directory representing the User's Desktop
+QString getDesktopDir();
 
-    // Create a shortcut at *location*, pointing to *dest* called with the arguments *args*
-    // call it *name* and assign it the icon *icon*
-    // return true if operation succeeded
-    bool createShortCut(QString location, QString dest, QStringList args, QString name, QString iconLocation);
+// Create a shortcut at *location*, pointing to *dest* called with the arguments *args*
+// call it *name* and assign it the icon *icon*
+// return true if operation succeeded
+bool createShortCut(QString location, QString dest, QStringList args, QString name, QString iconLocation);
 }
 
 #endif // USERUTILS_H
