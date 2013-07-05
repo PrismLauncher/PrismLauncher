5c07e1b042ecd22dfcd3ea8cf656c99549cc984b
Petr Mrázek <peterix@gmail.com>
2013-07-06 01:50:07 +0200

    Move job queue to libutil

---
+++ libutil/include/dlqueue.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "jobqueue.h"
+#include <QtNetwork>
+
+/**
+ * A single file for the downloader/cache to process.
+ */
+class DownloadJob : public Job
+{
+	Q_OBJECT
+public:
+	DownloadJob(QUrl url, QString rel_target_path = QString(), QString expected_md5 = QString());
+	static JobPtr create(QUrl url, QString rel_target_path = QString(), QString expected_md5 = QString());
+	
+public slots:
+	virtual void start();
+	
+private slots:
+	void downloadProgress(qint64 bytesReceived, qint64 bytesTotal);;
+	void downloadError(QNetworkReply::NetworkError error);
+	void downloadFinished();
+	void downloadReadyRead();
+	
+public:
+	/// the associated network manager
+	QSharedPointer<QNetworkAccessManager> m_manager;
+	/// the network reply
+	QSharedPointer<QNetworkReply> m_reply;
+	/// source URL
+	QUrl m_url;
+	
+	/// if true, check the md5sum against a provided md5sum
+	/// also, if a file exists, perform an md5sum first and don't download only if they don't match
+	bool m_check_md5;
+	/// the expected md5 checksum
+	QString m_expected_md5;
+	
+	/// save to file?
+	bool m_save_to_file;
+	/// if saving to file, use the one specified in this string
+	QString m_rel_target_path;
+	/// this is the output file, if any
+	QFile m_output_file;
+	/// if not saving to file, downloaded data is placed here
+	QByteArray m_data;
+	
+	/// The file's status
+	JobStatus m_status;
+};
---
+++ libutil/src/dlqueue.cpp
@@ -0,0 +1,128 @@
+#include "include/dlqueue.h"
+
+DownloadJob::DownloadJob ( QUrl url, QString rel_target_path, QString expected_md5 )
+	:Job()
+{
+	m_url = url;
+	m_rel_target_path = rel_target_path;
+	m_expected_md5 = expected_md5;
+
+	m_check_md5 = m_expected_md5.size();
+	m_save_to_file = m_rel_target_path.size();
+	m_status = Job_NotStarted;
+}
+
+JobPtr DownloadJob::create ( QUrl url, QString rel_target_path, QString expected_md5 )
+{
+	return JobPtr ( new DownloadJob ( url, rel_target_path, expected_md5 ) );
+}
+
+void DownloadJob::start()
+{
+	m_manager.reset ( new QNetworkAccessManager() );
+	if ( m_save_to_file )
+	{
+		QString filename = m_rel_target_path;
+		m_output_file.setFileName ( filename );
+		// if there already is a file and md5 checking is in effect
+		if ( m_output_file.exists() && m_check_md5 )
+		{
+			// and it can be opened
+			if ( m_output_file.open ( QIODevice::ReadOnly ) )
+			{
+				// check the md5 against the expected one
+				QString hash = QCryptographicHash::hash ( m_output_file.readAll(), QCryptographicHash::Md5 ).toHex().constData();
+				m_output_file.close();
+				// skip this file if they match
+				if ( hash == m_expected_md5 )
+				{
+					qDebug() << "Skipping " << m_url.toString() << ": md5 match.";
+					emit finish();
+					return;
+				}
+			}
+		}
+		QFileInfo a ( filename );
+		QDir dir;
+		if ( !dir.mkpath ( a.path() ) )
+		{
+			/*
+			 * error when making the folder structure
+			 */
+			emit fail();
+			return;
+		}
+		if ( !m_output_file.open ( QIODevice::WriteOnly ) )
+		{
+			/*
+			 * Can't open the file... the job failed
+			 */
+			emit fail();
+			return;
+		}
+	}
+	qDebug() << "Downloading " << m_url.toString();
+	QNetworkRequest request ( m_url );
+	QNetworkReply * rep = m_manager->get ( request );
+	m_reply = QSharedPointer<QNetworkReply> ( rep, &QObject::deleteLater );
+	connect ( rep, SIGNAL ( downloadProgress ( qint64,qint64 ) ), SLOT ( downloadProgress ( qint64,qint64 ) ) );
+	connect ( rep, SIGNAL ( finished() ), SLOT ( downloadFinished() ) );
+	connect ( rep, SIGNAL ( error ( QNetworkReply::NetworkError ) ), SLOT ( downloadError ( QNetworkReply::NetworkError ) ) );
+	connect ( rep, SIGNAL ( readyRead() ), SLOT ( downloadReadyRead() ) );
+}
+
+void DownloadJob::downloadProgress ( qint64 bytesReceived, qint64 bytesTotal )
+{
+	emit progress ( bytesReceived, bytesTotal );
+}
+
+void DownloadJob::downloadError ( QNetworkReply::NetworkError error )
+{
+	// error happened during download.
+	// TODO: log the reason why
+	m_status = Job_Failed;
+}
+
+void DownloadJob::downloadFinished()
+{
+	// if the download succeeded
+	if ( m_status != Job_Failed )
+	{
+		// nothing went wrong...
+		m_status = Job_Finished;
+		// save the data to the downloadable if we aren't saving to file
+		if ( !m_save_to_file )
+		{
+			m_data = m_reply->readAll();
+		}
+		else
+		{
+			m_output_file.close();
+		}
+
+		//TODO: check md5 here!
+		m_reply.clear();
+		emit finish();
+		return;
+	}
+	// else the download failed
+	else
+	{
+		if ( m_save_to_file )
+		{
+			m_output_file.close();
+			m_output_file.remove();
+		}
+		m_reply.clear();
+		emit fail();
+		return;
+	}
+}
+
+void DownloadJob::downloadReadyRead()
+{
+	if ( m_save_to_file )
+	{
+		m_output_file.write ( m_reply->readAll() );
+	}
+}
