ac2721e954142e534e3acced8a90ef6cf876efa7
Jan Dalheimer <jan@dalheimer.de>
2013-12-14 14:53:18 +0100

    Framework for some tests

---
+++ tests/tst_UpdateChecker.cpp
@@ -0,0 +1,22 @@
+#include <QTest>
+
+#include "TestUtil.h"
+
+class UpdateCheckerTest : public QObject
+{
+	Q_OBJECT
+private
+slots:
+	void initTestCase()
+	{
+
+	}
+	void cleanupTestCase()
+	{
+
+	}
+};
+
+QTEST_GUILESS_MAIN(UpdateCheckerTest)
+
+#include "tst_UpdateChecker.moc"
