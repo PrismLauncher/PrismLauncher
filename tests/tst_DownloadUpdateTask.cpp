0d7b411729b89c5cebc8baaf48bec46fb182536b
Petr Mrázek <peterix@gmail.com>
2013-12-25 03:25:21 +0100

    No more windows paths in tests.

--- tests/tst_DownloadUpdateTask.cpp
+++ tests/tst_DownloadUpdateTask.cpp
@@ -99,11 +99,7 @@ slots:
 		ops << DownloadUpdateTask::UpdateOperation::CopyOp("sourceOne", "destOne", 0777)
 			<< DownloadUpdateTask::UpdateOperation::CopyOp("MultiMC.exe", "M/u/l/t/i/M/C/e/x/e")
 			<< DownloadUpdateTask::UpdateOperation::DeleteOp("toDelete.abc");
-#if defined(Q_OS_WIN)
-		auto testFile = "tests/data/tst_DownloadUpdateTask-test_writeInstallScript_win32.xml";
-#else
 		auto testFile = "tests/data/tst_DownloadUpdateTask-test_writeInstallScript.xml";
-#endif
 		const QString script = QDir::temp().absoluteFilePath("MultiMCUpdateScript.xml");
 		QVERIFY(task.writeInstallScript(ops, script));
 		QCOMPARE(TestsInternal::readFileUtf8(script).replace(QRegExp("[\r\n]+"), "\n"),
