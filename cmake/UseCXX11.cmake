3f2152c14df019f441a48e2f9926c24b0aca0fd3
Jan Dalheimer <jan@dalheimer.de>
2014-04-12 18:11:30 +0200

    Let travis build a clang version

--- cmake/UseCXX11.cmake
+++ cmake/UseCXX11.cmake
@@ -4,7 +4,7 @@ endif()
 set(__USECXX11_CMAKE__ TRUE)
 
 if(APPLE)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++11")
 elseif(UNIX)
     # assume GCC, add C++0x/C++11 stuff
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
