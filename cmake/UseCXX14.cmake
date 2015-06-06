678da0b639603295169d4b15988ea0832a9f2e95
Petr Mrázek <peterix@gmail.com>
2015-06-06 19:56:51 +0200

    GH-1052 switch over to C++14 everywhere

---
+++ cmake/UseCXX14.cmake
@@ -0,0 +1,13 @@
+if(__USECXX14_CMAKE__)
+    return()
+endif()
+set(__USECXX14_CMAKE__ TRUE)
+
+if(APPLE)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -std=c++14")
+elseif(UNIX)
+    # assume GCC, add C++0x/C++11 stuff
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14")
+elseif(MINGW)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++14")
+endif()
