12b14c3400e11bb712efe760ed7a2cf46d2f75c7
Petr Mrázek <peterix@gmail.com>
2015-09-28 23:28:40 +0200

    GH-1179 incorporate fixes for comments from linked changeset
    Use SSL for downloads
    Use XDG dirs for storage
    Do not run from deploy function

--- application/package/ubuntu/multimc_1.1-1/opt/multimc/run.sh
+++ application/package/ubuntu/multimc_1.1-1/opt/multimc/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTDIR=~/.multimc
+INSTDIR=$XDG_DATA_HOME/multimc
 if [ `getconf LONG_BIT` = "64" ]
 then
     PACKAGE="mmc-stable-lin64.tar.gz"
@@ -12,12 +12,11 @@ deploy() {
     mkdir -p $INSTDIR
     cd ${INSTDIR}
 
-    wget --progress=dot:force "http://files.multimc.org/downloads/${PACKAGE}" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-close --auto-kill --title="Downloading MultiMC..."
+    wget --progress=dot:force "https://files.multimc.org/downloads/${PACKAGE}" 2>&1 | sed -u 's/.* \([0-9]\+%\)\ \+\([0-9.]\+.\) \(.*\)/\1\n# Downloading at \2\/s, ETA \3/' | zenity --progress --auto-close --auto-kill --title="Downloading MultiMC..."
 
     tar -xzf ${PACKAGE} --transform='s,MultiMC/,,'
     rm ${PACKAGE}
     chmod +x MultiMC
-    ./MultiMC
 }
 
 runmmc() {
@@ -27,6 +26,7 @@ runmmc() {
 
 if [[ ! -f ${INSTDIR}/MultiMC ]]; then
     deploy
+    runmmc
 else
     runmmc
 fi
