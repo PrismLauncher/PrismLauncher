0f2757f0004e20503b2c4ee587e16f6775ee14bf
AbigailBuccaneer <AbigailBuccaneer@users.noreply.github.com>
2020-08-24 17:43:58 +0100

    Remove extra semicolons from big-endian handling
    Fixes #3296.

--- libraries/classparser/src/javaendian.h
+++ libraries/classparser/src/javaendian.h
@@ -11,32 +11,32 @@ inline uint64_t bigswap(uint64_t x)
 {
     return x;
 }
-;
+
 inline uint32_t bigswap(uint32_t x)
 {
     return x;
 }
-;
+
 inline uint16_t bigswap(uint16_t x)
 {
     return x;
 }
-;
+
 inline int64_t bigswap(int64_t x)
 {
     return x;
 }
-;
+
 inline int32_t bigswap(int32_t x)
 {
     return x;
 }
-;
+
 inline int16_t bigswap(int16_t x)
 {
     return x;
 }
-;
+
 #else
 inline uint64_t bigswap(uint64_t x)
 {
