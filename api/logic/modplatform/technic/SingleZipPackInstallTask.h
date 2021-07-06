d5c448931344a6cee5f89fddf9a8e64dec1f8da6
Jamie Mansfield <jmansfield@cadixdev.org>
2021-07-06 15:12:39 +0100

    GH-1949 Allow Technic pack downloads to be aborted
    This supports both 'single zip' modpacks and Solder packs, through the
    Technic mod platform page.

--- api/logic/modplatform/technic/SingleZipPackInstallTask.h
+++ api/logic/modplatform/technic/SingleZipPackInstallTask.h
@@ -36,6 +36,9 @@ class MULTIMC_LOGIC_EXPORT SingleZipPackInstallTask : public InstanceTask
 public:
     SingleZipPackInstallTask(const QUrl &sourceUrl, const QString &minecraftVersion);
 
+    bool canAbort() const override { return true; }
+    bool abort() override;
+
 protected:
     void executeTask() override;
 
@@ -48,6 +51,8 @@ private slots:
     void extractAborted();
 
 private:
+    bool m_abortable = false;
+
     QUrl m_sourceUrl;
     QString m_minecraftVersion;
     QString m_archivePath;
