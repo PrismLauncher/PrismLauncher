4b13577a5924d84fc78111e5f857d5538af77fcd
Jamie Mansfield <jmansfield@cadixdev.org>
2021-10-24 00:39:02 +0100

    GH-4185 Include the modpack version in instance title for modpacks.ch

--- launcher/pages/modplatform/atlauncher/AtlPage.cpp
+++ launcher/pages/modplatform/atlauncher/AtlPage.cpp
@@ -70,7 +70,7 @@ void AtlPage::suggestCurrent()
         return;
     }
 
-    dialog->setSuggestedPack(selected.name, new ATLauncher::PackInstallTask(this, selected.safeName, selectedVersion));
+    dialog->setSuggestedPack(selected.name + " " + selectedVersion, new ATLauncher::PackInstallTask(this, selected.safeName, selectedVersion));
     auto editedLogoName = selected.safeName;
     auto url = QString(BuildConfig.ATL_DOWNLOAD_SERVER_URL + "launcher/images/%1.png").arg(selected.safeName.toLower());
     listModel->getLogo(selected.safeName, url, [this, editedLogoName](QString logo)
