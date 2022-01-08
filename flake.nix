8d184958d8412762a345aa1a5047db0cba03dfcb
Mustafa Çalışkan <muscaln@protonmail.com>
2022-01-08 21:33:50 +0300

    nix: init flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,32 @@
 {
-  description = "PolyMC";
+  description = "PolyMC flake";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = inputs: {
-    overlay = import ./packages/nix/overlay.nix;
-  };
+  outputs = inputs@{ self, nixpkgs, flake-utils,... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+ 
+        packages = {
+          polymc = pkgs.libsForQt5.callPackage ./package/nix {};
+        };
+        
+        apps = {
+          polymc = flake-utils.lib.mkApp {
+            name = "PolyMC";
+            drv = packages.polymc;
+          };
+        };
+      in
+      {
+        inherit packages apps;
+
+        defaultPackage = packages.polymc;
+        defaultApp = apps.polymc;
+        overlay = import ./packages/nix/overlay.nix;
+      }
+    );
 }
