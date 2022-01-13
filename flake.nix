f84fc783b614e42e54a3329bff2541fea697940e
meguminloli <meguminloli@protonmail.com>
2022-01-13 15:28:45 +0200

    removed redundant recs
    removed file

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "PolyMC flake";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.flake-compat = { 
+  inputs.flake-compat = {
     url = "github:edolstra/flake-compat";
     flake = false;
   };
@@ -16,29 +16,27 @@
   };
 
   outputs = args@{ self, nixpkgs, flake-utils, libnbtplusplus, quazip, ... }:
-  {
-    overlay = final: prev: {
-      inherit (self.packages.${final.system})
-      polymc;
-    };
-  } // flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux"] (system:
-    let pkgs = import nixpkgs {
-      inherit system;
-    };
-    in {
-      defaultPackage = self.packages.${system}.polymc;
-      packages = rec {
-        polymc = pkgs.libsForQt5.callPackage ./packages/nix/polymc {
-	  inherit self;
-	  submoduleQuazip = quazip;
-	  submoduleNbt = libnbtplusplus;
-	};
+    {
+      overlay = final: prev: {
+        inherit (self.packages.${final.system}) polymc;
       };
-      apps = rec {
-	polymc = flake-utils.lib.mkApp {
-	  name = "polymc";
-	  drv = self.packages.${system}.polymc;
-	};
-      };
-    });
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        packages = {
+          polymc = pkgs.libsForQt5.callPackage ./packages/nix/polymc {
+            inherit self;
+            submoduleQuazip = quazip;
+            submoduleNbt = libnbtplusplus;
+          };
+        };
+        apps = {
+          polymc = flake-utils.lib.mkApp {
+            name = "polymc";
+            drv = self.packages.${system}.polymc;
+          };
+        };
+        defaultPackage = self.packages.${system}.polymc;
+        defaultApp = self.apps.${system}.polymc;
+      });
 }
