159d868b77e6962632e925caf80a347122203d27
Sefa Eyeoglu <contact@scrumplex.net>
2022-02-12 20:30:42 +0100

    fix(ui): explain why 'Add Microsoft' might be disabled

--- launcher/ui/pages/global/AccountListPage.cpp
+++ launcher/ui/pages/global/AccountListPage.cpp
@@ -73,7 +73,10 @@ AccountListPage::AccountListPage(QWidget *parent)
     updateButtonStates();
 
     // Xbox authentication won't work without a client identifier, so disable the button if it is missing
-    ui->actionAddMicrosoft->setVisible(!APPLICATION->getMSAClientID().isEmpty());
+    if (APPLICATION->getMSAClientID().isEmpty()) {
+        ui->actionAddMicrosoft->setVisible(false);
+        ui->actionAddMicrosoft->setToolTip(tr("No Microsoft Secured Account client ID was set."));
+    }
 }
 
 AccountListPage::~AccountListPage()
