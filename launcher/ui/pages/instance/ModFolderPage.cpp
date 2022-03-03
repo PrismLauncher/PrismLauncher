e0c025b162fe44fb29951d4bd55e675ae504c871
flow <thiagodonato300@gmail.com>
2022-03-03 09:51:46 -0300

    fix extra spacing in resource packs and shader packs, and move button up
    hopefully now its finally ok

--- launcher/ui/pages/instance/ModFolderPage.cpp
+++ launcher/ui/pages/instance/ModFolderPage.cpp
@@ -143,17 +143,19 @@ ModFolderPage::ModFolderPage(
     ui(new Ui::ModFolderPage)
 {
     ui->setupUi(this);
-    auto act = ui->actionDownload;
+
+    // This is structured like that so that these changes
+    // do not affect the Resouce pack and Shader pack tabs
     if(id == "mods") {
+        auto act = new QAction(tr("Download mods"), this);
+        act->setToolTip(tr("Download mods from online mod platforms"));
+        ui->actionsToolbar->insertActionBefore(ui->actionAdd, act);
         connect(act, &QAction::triggered, this, &ModFolderPage::on_actionInstall_mods_triggered);
+
+        ui->actionAdd->setText("Add .jar");
+        ui->actionAdd->setToolTip("Add mods via local file");
     }
-    else{
-        // HACK: Prevent the download button from showing in the shaders / resource packs tab
-        // This whole thing needs some cleaning up anyway, no next time we can do it properly...
-        act->setVisible(false);
-        act->setText("");
-        act->setToolTip("");
-    }
+
     ui->actionsToolbar->insertSpacer(ui->actionView_configs);
 
     m_inst = inst;
