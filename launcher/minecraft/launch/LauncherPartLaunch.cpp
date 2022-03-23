b1af689546704d5e05ef4bc44f43ce9e8bb4854c
dada513 <dada513@protonmail.com>
2022-03-23 19:06:17 +0100

    Add quit launcher after game stops option (Steam Deck)
    lecense

--- launcher/minecraft/launch/LauncherPartLaunch.cpp
+++ launcher/minecraft/launch/LauncherPartLaunch.cpp
@@ -170,6 +170,7 @@ void LauncherPartLaunch::on_state(LoggedProcess::State state)
         {
             if (APPLICATION->settings()->get("CloseAfterLaunch").toBool())
                 APPLICATION->showMainWindow();
+
             m_parent->setPid(-1);
             // if the exit code wasn't 0, report this as a crash
             auto exitCode = m_process.exitCode();
