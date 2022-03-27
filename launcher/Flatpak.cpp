6a180f495f28db0ae254a17112618291369e86a2
dada513 <dada513@protonmail.com>
2022-03-27 14:42:02 +0200

    more flatpak fixes

--- launcher/Flatpak.cpp
+++ launcher/Flatpak.cpp
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  PolyMC - Minecraft Launcher
+ *  Copyright (C) 2022 dada513 <dada513@protonmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include <QFileInfo>
 
 namespace Flatpak
@@ -10,5 +27,5 @@ namespace Flatpak
      #else
      return false;
      #endif
-   } 
+    } 
 }
