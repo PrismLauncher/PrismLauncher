2fc1b999117ceebc3ebf05d96b0a749e3a0f9e98
flow <thiagodonato300@gmail.com>
2022-05-10 19:57:47 -0300

    chore: add license headers
    Prevents a massive inload of Scrumplex ditto's :)
    
    I didn't add it to every file modified in this PR because the other
    changes are pretty minor, and would explode the diff of the PR. I hope
    that's not a problem O_O

--- launcher/ModDownloadTask.cpp
+++ launcher/ModDownloadTask.cpp
@@ -1,7 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+*  PolyMC - Minecraft Launcher
+*  Copyright (c) 2022 flowln <flowlnlnln@gmail.com>
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, version 3.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "ModDownloadTask.h"
 
 #include "Application.h"
-#include "minecraft/mod/tasks/LocalModUpdateTask.h"
+#include "minecraft/mod/ModFolderModel.h"
 
 ModDownloadTask::ModDownloadTask(ModPlatform::IndexedPack mod, ModPlatform::IndexedVersion version, const std::shared_ptr<ModFolderModel> mods)
     : m_mod(mod), m_mod_version(version), mods(mods)
--- launcher/ModDownloadTask.h
+++ launcher/ModDownloadTask.h
@@ -1,13 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+*  PolyMC - Minecraft Launcher
+*  Copyright (c) 2022 flowln <flowlnlnln@gmail.com>
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, version 3.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
-#include <QUrl>
-#include "QObjectPtr.h"
-#include "minecraft/mod/ModFolderModel.h"
-#include "modplatform/ModIndex.h"
 #include "net/NetJob.h"
-
 #include "tasks/SequentialTask.h"
+
+#include "modplatform/ModIndex.h"
 #include "minecraft/mod/tasks/LocalModUpdateTask.h"
+
+class ModFolderModel;
 
 class ModDownloadTask : public SequentialTask {
     Q_OBJECT
