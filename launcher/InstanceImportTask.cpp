5f2398fe59b0053d94c0600f54bddc642751bf74
flow <thiagodonato300@gmail.com>
2022-05-15 08:25:58 -0300

    chore: license headers 2

--- launcher/InstanceImportTask.cpp
+++ launcher/InstanceImportTask.cpp
@@ -2,6 +2,7 @@
 /*
  *  PolyMC - Minecraft Launcher
  *  Copyright (C) 2022 Sefa Eyeoglu <contact@scrumplex.net>
+ *  Copyright (c) 2022 flowln <flowlnlnln@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- launcher/ui/pages/modplatform/modrinth/ModrinthModel.cpp
+++ launcher/ui/pages/modplatform/modrinth/ModrinthModel.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
  *  PolyMC - Minecraft Launcher
+ *  Copyright (c) 2022 flowln <flowlnlnln@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
