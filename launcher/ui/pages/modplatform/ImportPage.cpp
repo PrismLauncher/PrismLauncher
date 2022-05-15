3abf466632588f9285579a9822b5da2c9fea7bec
Sefa Eyeoglu <contact@scrumplex.net>
2022-05-15 13:20:05 +0200

    chore: add/update license headers

--- launcher/ui/pages/modplatform/ImportPage.cpp
+++ launcher/ui/pages/modplatform/ImportPage.cpp
@@ -2,6 +2,7 @@
 /*
  *  PolyMC - Minecraft Launcher
  *  Copyright (c) 2022 Jamie Mansfield <jmansfield@cadixdev.org>
+ *  Copyright (c) 2022 Sefa Eyeoglu <contact@scrumplex.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
