7a5a1a9af7e0c4d31c0ae1fa23de58cb93dbec45
Sefa Eyeoglu <contact@scrumplex.net>
2022-06-01 17:46:12 +0200

    Merge pull request #692 from glowiak/patch-6

--- launcher/tools/MCEditTool.cpp
+++ launcher/tools/MCEditTool.cpp
@@ -52,7 +52,7 @@ QString MCEditTool::getProgramPath()
 #else
     const QString mceditPath = path();
     QDir mceditDir(mceditPath);
-#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD) || defined(Q_OS_OPENBSD)
+#if defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD)
     if (mceditDir.exists("mcedit.sh"))
     {
         return mceditDir.absoluteFilePath("mcedit.sh");
