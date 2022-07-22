87a0482b8b299fd54691e3042ca661863ea6290a
flow <flowlnlnln@gmail.com>
2022-07-21 22:40:06 -0300

    refactor: make MultipleOptionsTask inherit from ConcurrentTask too
    Signed-off-by: flow <flowlnlnln@gmail.com>

--- launcher/tasks/MultipleOptionsTask.h
+++ launcher/tasks/MultipleOptionsTask.h
@@ -1,19 +1,17 @@
 #pragma once
 
-#include "SequentialTask.h"
+#include "ConcurrentTask.h"
 
 /* This task type will attempt to do run each of it's subtasks in sequence,
  * until one of them succeeds. When that happens, the remaining tasks will not run.
  * */
-class MultipleOptionsTask : public SequentialTask
-{
+class MultipleOptionsTask : public ConcurrentTask {
     Q_OBJECT
-public:
-    explicit MultipleOptionsTask(QObject *parent = nullptr, const QString& task_name = "");
-    virtual ~MultipleOptionsTask() = default;
+   public:
+    explicit MultipleOptionsTask(QObject* parent = nullptr, const QString& task_name = "");
+    ~MultipleOptionsTask() override = default;
 
-private
-slots:
+   private slots:
     void startNext() override;
-    void subTaskFailed(const QString &msg) override;
+    void updateState() override;
 };
