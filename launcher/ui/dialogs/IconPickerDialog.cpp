5f190ca34c2e1c3423cb30d2d1ccc355aac685d6
Sefa Eyeoglu <contact@scrumplex.net>
2022-10-03 15:47:02 +0200

    fix: fix layout of icon picker
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- launcher/ui/dialogs/IconPickerDialog.cpp
+++ launcher/ui/dialogs/IconPickerDialog.cpp
@@ -22,12 +22,14 @@
 #include "IconPickerDialog.h"
 #include "ui_IconPickerDialog.h"
 
+#include "ui/instanceview/InstanceDelegate.h"
+
 #include "icons/IconList.h"
 #include "icons/IconUtils.h"
 #include <DesktopServices.h>
 
-IconPickerDialog::IconPickerDialog(QWidget *parent)
-    : QDialog(parent), ui(new Ui::IconPickerDialog)
+IconPickerDialog::IconPickerDialog(QWidget *parent, int iconSize)
+    : QDialog(parent), ui(new Ui::IconPickerDialog), m_iconSize(iconSize)
 {
     ui->setupUi(this);
     setWindowModality(Qt::WindowModal);
@@ -39,14 +41,17 @@ IconPickerDialog::IconPickerDialog(QWidget *parent)
     contentsWidget->setMovement(QListView::Static);
     contentsWidget->setResizeMode(QListView::Adjust);
     contentsWidget->setSelectionMode(QAbstractItemView::SingleSelection);
-    contentsWidget->setSpacing(5);
-    contentsWidget->setWordWrap(false);
+    contentsWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+    contentsWidget->setWordWrap(true);
     contentsWidget->setWrapping(true);
     contentsWidget->setUniformItemSizes(true);
     contentsWidget->setTextElideMode(Qt::ElideRight);
     contentsWidget->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
     contentsWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
-   contentsWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    contentsWidget->setFrameStyle(QFrame::NoFrame);
+    contentsWidget->setGridSize(QSize(m_iconSize * 2, m_iconSize * 2));
+    contentsWidget->setItemDelegate(new InstanceDelegate(this, m_iconSize, true));
+    contentsWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     // contentsWidget->setAcceptDrops(true);
     contentsWidget->setDropIndicatorShown(true);
--- launcher/ui/dialogs/IconPickerDialog.h
+++ launcher/ui/dialogs/IconPickerDialog.h
@@ -27,7 +27,7 @@ class IconPickerDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit IconPickerDialog(QWidget *parent = 0);
+    explicit IconPickerDialog(QWidget *parent = 0, int iconSize = 48);
     ~IconPickerDialog();
     int execWithSelection(QString selection);
     QString selectedIconKey;
@@ -37,6 +37,7 @@ protected:
 
 private:
     Ui::IconPickerDialog *ui;
+    int m_iconSize;
 
 private
 slots:
