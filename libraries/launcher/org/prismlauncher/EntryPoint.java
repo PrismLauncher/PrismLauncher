c0b8c53e6960de48c6a43796b413d1780f681a3a
TheKodeToad <TheKodeToad@proton.me>
2022-10-29 15:05:43 +0100

    Attempt to sort out copyright headers
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

--- libraries/launcher/org/prismlauncher/EntryPoint.java
+++ libraries/launcher/org/prismlauncher/EntryPoint.java
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
  *  PolyMC - Minecraft Launcher
- *  Copyright (C) 2022 icelimetea, <fr3shtea@outlook.com>
+ *  Copyright (C) 2022 icelimetea <fr3shtea@outlook.com>
+ *  Copyright (C) 2022 TheKodeToad <TheKodeToad@proton.me>
+ *  Copyright (C) 2022 solonovamax <solonovamax@12oclockpoint.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- libraries/launcher/org/prismlauncher/launcher/LauncherFactory.java
+++ libraries/launcher/org/prismlauncher/launcher/LauncherFactory.java
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
- *  Prism Launcher - Minecraft Launcher
- *  Copyright (C) 2022 icelimetea, <fr3shtea@outlook.com>
+ *  PolyMC - Minecraft Launcher
+ *  Copyright (C) 2022 icelimetea <fr3shtea@outlook.com>
+ *  Copyright (C) 2022 Sefa Eyeoglu <contact@scrumplex.net>
+ *  Copyright (C) 2022 Samisafool <thenerdiestguy@gmail.com>
+ *  Copyright (C) 2022 TheKodeToad <TheKodeToad@proton.me>
+ *  Copyright (C) 2022 solonovamax <solonovamax@12oclockpoint.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -31,6 +35,23 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ *      Copyright 2013-2021 MultiMC Contributors
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
  */
 
 package org.prismlauncher.launcher;
@@ -73,4 +94,5 @@ public final class LauncherFactory {
 
         return launcherProvider.provide(parameters);
     }
+
 }
