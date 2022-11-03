ab6c7244fc472de0bed761cf29700a96dd89e8ad
flow <flowlnlnln@gmail.com>
2022-11-03 16:44:23 -0300

    refactor: move FS's toStdString to StringUtils
    This is so that anyone can use it :)
    
    Signed-off-by: flow <flowlnlnln@gmail.com>

--- launcher/StringUtils.h
+++ launcher/StringUtils.h
@@ -3,5 +3,18 @@
 #include <QString>
 
 namespace StringUtils {
+
+#if defined Q_OS_WIN32
+inline std::wstring toStdString(QString s)
+{
+    return s.toStdWString();
+}
+#else
+inline std::string toStdString(QString s)
+{
+    return s.toStdString();
+}
+#endif
+
 int naturalCompare(const QString& s1, const QString& s2, Qt::CaseSensitivity cs);
 }  // namespace StringUtils
