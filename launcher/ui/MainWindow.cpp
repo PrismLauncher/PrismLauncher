d5d224d89a6d1fccacbf8ffe8d0981cec32e89fb
Sefa Eyeoglu <contact@scrumplex.net>
2022-11-06 01:05:10 +0100

    fix: save metacache after clearing
    If the user closes the launcher right after clearing, it probably
    didn't actually clear the cache yet.
    
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- launcher/ui/MainWindow.cpp
+++ launcher/ui/MainWindow.cpp
@@ -1899,6 +1899,7 @@ void MainWindow::on_actionReportBug_triggered()
 void MainWindow::on_actionClearMetadata_triggered()
 {
     APPLICATION->metacache()->evictAll();
+    APPLICATION->metacache()->SaveNow();
 }
 
 void MainWindow::on_actionOpenWiki_triggered()
