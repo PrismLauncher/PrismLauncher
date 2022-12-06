PolyMC/PolyMC

refactor: abstract away update confirmation dialog
... so that we can avoid code duplication.

Signed-off-by: flow <flowlnlnln@gmail.com>

launcher/InstanceTask.cpp
@@ -18,6 +18,29 @@ InstanceNameChange askForChangingInstanceName(QWidget* parent, const QString& ol
     return InstanceNameChange::ShouldKeep;
 }
 
+ShouldUpdate askIfShouldUpdate(QWidget *parent, QString original_version_name)
+{
+    auto info = CustomMessageBox::selectable(
+        parent, QObject::tr("Similar modpack was found!"),
+        QObject::tr("One or more of your instances are from this same modpack%1. Do you want to create a "
+           "separate instance, or update the existing one?\n\nNOTE: Make sure you made a backup of your important instance data before "
+           "updating, as worlds can be corrupted and some configuration may be lost (due to pack overrides).")
+            .arg(original_version_name),
+        QMessageBox::Information, QMessageBox::Ok | QMessageBox::Reset | QMessageBox::Abort);
+    info->setButtonText(QMessageBox::Ok, QObject::tr("Create new instance"));
+    info->setButtonText(QMessageBox::Abort, QObject::tr("Update existing instance"));
+    info->setButtonText(QMessageBox::Reset, QObject::tr("Cancel"));
+
+    info->exec();
+
+    if (info->clickedButton() == info->button(QMessageBox::Ok))
+        return ShouldUpdate::SkipUpdating;
+    if (info->clickedButton() == info->button(QMessageBox::Abort))
+        return ShouldUpdate::Update;
+    return ShouldUpdate::Cancel;
+
+}
+
 QString InstanceName::name() const
 {
     if (!m_modified_name.isEmpty())

PrismLauncher/PrismLauncher

launcher/InstanceTask.cpp
@@ -18,6 +18,29 @@ InstanceNameChange askForChangingInstanceName(QWidget* parent, const QString& ol
     return InstanceNameChange::ShouldKeep;
 }
 
+ShouldUpdate askIfShouldUpdate(QWidget *parent, QString original_version_name)
+{
+    auto info = CustomMessageBox::selectable(
+        parent, QObject::tr("Similar modpack was found!"),
+        QObject::tr("One or more of your instances are from this same modpack%1. Do you want to create a "
+           "separate instance, or update the existing one?\n\nNOTE: Make sure you made a backup of your important instance data before "
+           "updating, as worlds can be corrupted and some configuration may be lost (due to pack overrides).")
+            .arg(original_version_name),
+        QMessageBox::Information, QMessageBox::Ok | QMessageBox::Reset | QMessageBox::Abort);
+    info->setButtonText(QMessageBox::Ok, QObject::tr("Update existing instance"));
+    info->setButtonText(QMessageBox::Abort, QObject::tr("Create new instance"));
+    info->setButtonText(QMessageBox::Reset, QObject::tr("Cancel"));
+
+    info->exec();
+
+    if (info->clickedButton() == info->button(QMessageBox::Ok))
+        return ShouldUpdate::Update;
+    if (info->clickedButton() == info->button(QMessageBox::Abort))
+        return ShouldUpdate::SkipUpdating;
+    return ShouldUpdate::Cancel;
+
+}
+
 QString InstanceName::name() const
 {
     if (!m_modified_name.isEmpty())