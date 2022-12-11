8bcae237964ac2c517f726ad82fd91019b2e5488
Aresiel <love.dittlau@gmail.com>
2022-12-12 00:05:00 +0100

    Grammar improvements
    Signed-off-by: Aresiel <love.dittlau@gmail.com>

--- launcher/java/JavaChecker.cpp
+++ launcher/java/JavaChecker.cpp
@@ -153,7 +153,7 @@ void JavaChecker::finished(int exitcode, QProcess::ExitStatus status)
     for(QString line : lines)
     {
         line = line.trimmed();
-        // NOTE: workaround for GH-4125, where garbage is getting printed into stdout on bedrock linux
+        // NOTE: Workaround for GH-4125, where garbage is getting printed into stdout on bedrock linux
         if (line.contains("/bedrock/strata")) {
             continue;
         }
