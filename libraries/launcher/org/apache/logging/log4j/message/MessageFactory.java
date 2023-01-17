02256ba62d30b698e8b8002ae9a8c1f4223eee03
PandaNinjas <admin@malwarefight.gq>
2023-01-17 14:25:36 -0800

    Change to using log4j configuration files.
    Signed-off-by: PandaNinjas <admin@malwarefight.gq>

--- libraries/launcher/org/apache/logging/log4j/message/MessageFactory.java
+++
@@ -1,7 +0,0 @@
-package org.apache.logging.log4j.message;
-
-/**
- * This class is a stub and is needed to compile the log4j injector
- * Changing it may break compilation
- */
-public class MessageFactory {}
