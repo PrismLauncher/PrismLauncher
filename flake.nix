950f921c09930076a19621bea9730cc866c3c482
Sefa Eyeoglu <contact@scrumplex.net>
2023-03-14 10:50:07 +0100

    refactor(nix): use flake-utils
    Signed-off-by: Sefa Eyeoglu <contact@scrumplex.net>

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
@@ -16,6 +17,7 @@
   outputs = {
     self,
     nixpkgs,
+    flake-utils,
     libnbtplusplus,
     ...
   }: let
@@ -23,26 +25,29 @@
     version = builtins.substring 0 8 self.lastModifiedDate;
 
     # Supported systems (qtbase is currently broken for "aarch64-darwin")
-    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
-
-    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-    # Nixpkgs instantiated for supported systems.
-    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    supportedSystems = with flake-utils.lib.system; [
+      x86_64-linux
+      x86_64-darwin
+      aarch64-linux
+    ];
 
     packagesFn = pkgs: rec {
-      prismlauncher-qt5 = pkgs.libsForQt5.callPackage ./nix {inherit version self libnbtplusplus;};
-      prismlauncher = pkgs.qt6Packages.callPackage ./nix {inherit version self libnbtplusplus;};
+      prismlauncher-qt5 = pkgs.libsForQt5.callPackage ./nix {
+        inherit version self libnbtplusplus;
+      };
+      prismlauncher = pkgs.qt6Packages.callPackage ./nix {
+        inherit version self libnbtplusplus;
+      };
     };
-  in {
-    packages = forAllSystems (
-      system: let
-        packages = packagesFn pkgs.${system};
+  in
+    flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = let
+        packages = packagesFn pkgs;
       in
-        packages // {default = packages.prismlauncher;}
-    );
+        packages // {default = packages.prismlauncher;};
 
-    overlay = final: packagesFn;
-  };
+      overlay = final: packagesFn;
+    });
 }
