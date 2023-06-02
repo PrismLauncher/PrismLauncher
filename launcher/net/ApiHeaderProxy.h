24bbc044679e0f843502b8238563258c5dc545fe
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-06-01 20:31:23 -0700

    chore: format
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

--- launcher/net/ApiHeaderProxy.h
+++ launcher/net/ApiHeaderProxy.h
@@ -19,9 +19,9 @@
 
 #pragma once
 
-#include "net/HeaderProxy.h"
 #include "Application.h"
 #include "BuildConfig.h"
+#include "net/HeaderProxy.h"
 
 namespace Net {
 
@@ -31,19 +31,19 @@ class ApiHeaderProxy : public HeaderProxy {
     virtual ~ApiHeaderProxy() = default;
 
    public:
-    virtual QList<HeaderPair> headers(const QNetworkRequest& request) const override { 
-      QList<HeaderPair> hdrs;  
-      if (APPLICATION->capabilities() & Application::SupportsFlame && request.url().host() == QUrl(BuildConfig.FLAME_BASE_URL).host()) {
-          hdrs.append({ "x-api-key", APPLICATION->getFlameAPIKey().toUtf8() });
-      } else if (request.url().host() == QUrl(BuildConfig.MODRINTH_PROD_URL).host() ||
-               request.url().host() == QUrl(BuildConfig.MODRINTH_STAGING_URL).host()) {
-          QString token = APPLICATION->getModrinthAPIToken();
-          if (!token.isNull())
-              hdrs.append({ "Authorization", token.toUtf8() });
-      }
-    return hdrs; 
+    virtual QList<HeaderPair> headers(const QNetworkRequest& request) const override
+    {
+        QList<HeaderPair> hdrs;
+        if (APPLICATION->capabilities() & Application::SupportsFlame && request.url().host() == QUrl(BuildConfig.FLAME_BASE_URL).host()) {
+            hdrs.append({ "x-api-key", APPLICATION->getFlameAPIKey().toUtf8() });
+        } else if (request.url().host() == QUrl(BuildConfig.MODRINTH_PROD_URL).host() ||
+                   request.url().host() == QUrl(BuildConfig.MODRINTH_STAGING_URL).host()) {
+            QString token = APPLICATION->getModrinthAPIToken();
+            if (!token.isNull())
+                hdrs.append({ "Authorization", token.toUtf8() });
+        }
+        return hdrs;
     };
-
 };
 
 }  // namespace Net
