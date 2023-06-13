b77fb059083ba04a3ccbc1b6e9ce4f5353b200ad
Trial97 <alexandru.tripon97@gmail.com>
2023-06-13 21:07:05 +0300

    Added back the INIFile read function
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- tests/INIFile_test.cpp
+++ tests/INIFile_test.cpp
@@ -1,24 +1,16 @@
 #include <QTest>
 
+#include <settings/INIFile.h>
 #include <QList>
 #include <QVariant>
-#include <settings/INIFile.h>
 
 #include <QVariantUtils.h>
 
-class IniFileTest : public QObject
-{
+class IniFileTest : public QObject {
     Q_OBJECT
-private
-slots:
-    void initTestCase()
-    {
-
-    }
-    void cleanupTestCase()
-    {
-
-    }
+   private slots:
+    void initTestCase() {}
+    void cleanupTestCase() {}
 
     void test_Escape_data()
     {
@@ -47,17 +39,17 @@ slots:
         // load
         INIFile f2;
         f2.loadFile(filename);
-        QCOMPARE(f2.get("a","NOT SET").toString(), a);
-        QCOMPARE(f2.get("b","NOT SET").toString(), b);
+        QCOMPARE(f2.get("a", "NOT SET").toString(), a);
+        QCOMPARE(f2.get("b", "NOT SET").toString(), b);
     }
 
     void test_SaveLoadLists()
     {
         QString slist_strings = "(\"a\",\"b\",\"c\")";
-        QStringList list_strings = {"a", "b", "c"};
+        QStringList list_strings = { "a", "b", "c" };
 
         QString slist_numbers = "(1,2,3,10)";
-        QList<int> list_numbers = {1, 2, 3, 10};
+        QList<int> list_numbers = { 1, 2, 3, 10 };
 
         QString filename = "test_SaveLoadLists.ini";
 
@@ -72,12 +64,40 @@ slots:
 
         QStringList out_list_strings = f2.get("list_strings", QStringList()).toStringList();
         qDebug() << "OutStringList" << out_list_strings;
-        
+
         QList<int> out_list_numbers = QVariantUtils::toList<int>(f2.get("list_numbers", QVariantUtils::fromList(QList<int>())));
         qDebug() << "OutNumbersList" << out_list_numbers;
 
         QCOMPARE(out_list_strings, list_strings);
         QCOMPARE(out_list_numbers, list_numbers);
+    }
+
+    void test_SaveAleardyExistingFile()
+    {
+        QString fileName = "test_SaveAleardyExistingFile.ini";
+        QString fileContent = R"(InstanceType=OneSix
+iconKey=vanillia_icon
+name=Minecraft Vanillia
+OverrideCommands=true
+PreLaunchCommand="$INST_JAVA" -jar packwiz-installer-bootstrap.jar link
+)";
+        QFile file(fileName);
+
+        if (file.open(QFile::WriteOnly | QFile::Text)) {
+            QTextStream stream(&file);
+            stream << fileContent;
+            file.close();
+        }
+
+        // load
+        INIFile f1;
+        f1.loadFile(fileName);
+        QCOMPARE(f1.get("PreLaunchCommand", "NOT SET").toString(), "\"$INST_JAVA\" -jar packwiz-installer-bootstrap.jar link");
+        f1.saveFile(fileName);
+        INIFile f2;
+        f2.loadFile(fileName);
+        QCOMPARE(f2.get("PreLaunchCommand", "NOT SET").toString(), "\"$INST_JAVA\" -jar packwiz-installer-bootstrap.jar link");
+        QCOMPARE(f2.get("ConfigVersion", "NOT SET").toString(), "1.1");
     }
 };
 
