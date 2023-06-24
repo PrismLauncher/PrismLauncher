df932c65875a20fd95a47c3394a802d131e93993
Trial97 <alexandru.tripon97@gmail.com>
2023-06-24 20:22:18 +0300

    Updated authors string in modlist
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/modplatform/flame/FlamePackExportTask.cpp
+++ launcher/modplatform/flame/FlamePackExportTask.cpp
@@ -36,7 +36,7 @@
 #include "modplatform/helpers/HashUtils.h"
 #include "tasks/Task.h"
 
-const QString FlamePackExportTask::TEMPLATE = "<li><a href={url}>{name} (by {authors})</a></li>";
+const QString FlamePackExportTask::TEMPLATE = "<li><a href={url}>{name}{authors}</a></li>";
 
 FlamePackExportTask::FlamePackExportTask(const QString& name,
                                          const QString& version,
@@ -338,11 +338,12 @@ void FlamePackExportTask::buildZip()
         QString content = "";
         for (auto mod : resolvedFiles) {
             if (mod.isMod) {
-                content += QString(TEMPLATE)
-                               .replace("{name}", mod.name)
-                               .replace("{url}", ModPlatform::getMetaURL(ModPlatform::ResourceProvider::FLAME, mod.slug))
-                               .replace("{authors}", mod.authors) +
-                           "\n";
+                auto line = QString(TEMPLATE)
+                                .replace("{name}", mod.name)
+                                .replace("{url}", ModPlatform::getMetaURL(ModPlatform::ResourceProvider::FLAME, mod.slug));
+                if (!mod.authors.isEmpty())
+                    line = line.replace("{authors}", QString(" (by {%1})").arg(mod.authors));
+                content += line + "\n";
             }
         }
         content = "<ul>" + content + "</ul>";
