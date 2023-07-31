d4137e5b747cc549f58640ea5a9426daa5fcdd57
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-07-31 00:52:27 -0700

    feat(windows builds): add CMakePresets.json and `installqt.ps1` - with default windows builds, make setting up even in visual studio a brease. - install qt on windows to `./libraries/Qt`
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

---
+++ installQT.ps1
@@ -0,0 +1,23 @@
+$aqt_url = "https://github.com/miurahr/aqtinstall/releases/latest/download/aqt.exe"
+$qt_path = ( Join-Path -Path "$PSScriptRoot/libraries" -ChildPath "Qt" )
+New-Item -ItemType Directory -Path $qt_path -ErrorAction SilentlyContinue
+$aqt_path = ( Join-Path -Path $qt_path -ChildPath "aqt.exe" )
+
+if (!([System.IO.File]::Exists($aqt_path))) {
+  Start-BitsTransfer -Source $aqt_url -Destination $aqt_path
+}
+# Invoke-WebRequest -Uri $aqt_url -OutFile $aqt_path
+#
+$qt_ver = "6.5.1"
+$system_arch = (Get-CimInstance -Class Win32_ComputerSystem).SystemType
+$qt_arch = Switch ($system_arch) {
+  {$_ -match "x64" } { "win64_msvc2019_64" }
+  {$_ -match "ARM" } { "win64_msvc2019_arm64" }
+}
+
+$aqt_install_args = @("install-qt", "windows", "desktop", $qt_ver, $qt_arch, "-m", "all", "--outputdir", $qt_path )
+
+# call_aqt $aqt_install_args
+$p = Start-Process -FilePath $aqt_path -ArgumentList $aqt_install_args -Wait
+
+
