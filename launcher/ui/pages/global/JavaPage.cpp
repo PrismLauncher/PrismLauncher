3574d89e0f6ea9d507e5c4844d07a2aeac9ade97
Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>
2023-08-22 16:16:50 +0300

    Update launcher/ui/pages/global/JavaPage.cpp
    Co-authored-by: Tayou <31988415+TayouVR@users.noreply.github.com>
    Signed-off-by: Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>

--- launcher/ui/pages/global/JavaPage.cpp
+++ launcher/ui/pages/global/JavaPage.cpp
@@ -197,7 +197,7 @@ void JavaPage::updateThresholds()
         ui->labelMaxMemIcon->setToolTip(tr("Your maximum memory allocation approaches your system memory capacity."));
     } else if (maxMem < minMem) {
         iconName = "status-yellow";
-        ui->labelMaxMemIcon->setToolTip(tr("Your maximum memory allocation is smaller that the minimum value"));
+        ui->labelMaxMemIcon->setToolTip(tr("Your maximum memory allocation is smaller than the minimum value"));
     } else {
         iconName = "status-good";
         ui->labelMaxMemIcon->setToolTip("");
