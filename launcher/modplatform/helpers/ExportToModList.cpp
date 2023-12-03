657416fe30c96a7bfb3978e8f84c5d0c7951ba27
Trial97 <alexandru.tripon97@gmail.com>
2023-12-03 21:29:02 +0200

    Removed header
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/modplatform/helpers/ExportToModList.cpp
+++ launcher/modplatform/helpers/ExportToModList.cpp
@@ -16,7 +16,6 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "ExportToModList.h"
-#include <qcontainerfwd.h>
 #include <QJsonArray>
 #include <QJsonDocument>
 #include <QJsonObject>
