1130fe851012b3d7deff207db495b455501e17fc
TheKodeToad <TheKodeToad@proton.me>
2024-01-02 17:57:24 +0000

    Happy new year :D
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

--- launcher/ui/dialogs/AboutDialog.cpp
+++ launcher/ui/dialogs/AboutDialog.cpp
@@ -174,8 +174,7 @@ AboutDialog::AboutDialog(QWidget* parent) : QDialog(parent), ui(new Ui::AboutDia
     QString urlText("<html><head/><body><p><a href=\"%1\">%1</a></p></body></html>");
     ui->urlLabel->setText(urlText.arg(BuildConfig.LAUNCHER_GIT));
 
-    QString copyText("© 2022-2023 %1");
-    ui->copyLabel->setText(copyText.arg(BuildConfig.LAUNCHER_COPYRIGHT));
+    ui->copyLabel->setText(BuildConfig.LAUNCHER_COPYRIGHT);
 
     connect(ui->closeButton, SIGNAL(clicked()), SLOT(close()));
 
