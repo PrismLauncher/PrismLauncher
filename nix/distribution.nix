6035b7562c6a3117dde6f397e7f5b43941ae4b4f
seth <getchoo@tuta.io>
2024-01-17 23:13:21 -0500

    refactor(nix): use revision for version
    Signed-off-by: seth <getchoo@tuta.io>

--- nix/distribution.nix
+++ nix/distribution.nix
@@ -18,32 +18,29 @@
         prismlauncher-unwrapped
         prismlauncher
         ;
+
       default = ourPackages.prismlauncher;
     };
   };
 
-  flake = {
-    overlays.default = final: prev: let
-      version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
+  flake.overlays.default = final: prev: let
+    # common args for prismlauncher evaluations
+    unwrappedArgs = {
+      version = builtins.substring 0 7 self.rev or "dirty";
+      inherit (inputs) libnbtplusplus;
+      inherit ((final.darwin or prev.darwin).apple_sdk.frameworks) Cocoa;
+    };
+  in {
+    prismlauncher-qt5-unwrapped = prev.libsForQt5.callPackage ./pkg unwrappedArgs;
 
-      # common args for prismlauncher evaluations
-      unwrappedArgs = {
-        inherit (inputs) libnbtplusplus;
-        inherit ((final.darwin or prev.darwin).apple_sdk.frameworks) Cocoa;
-        inherit version;
-      };
-    in {
-      prismlauncher-qt5-unwrapped = prev.libsForQt5.callPackage ./pkg unwrappedArgs;
+    prismlauncher-qt5 = prev.libsForQt5.callPackage ./pkg/wrapper.nix {
+      prismlauncher-unwrapped = final.prismlauncher-qt5-unwrapped;
+    };
 
-      prismlauncher-qt5 = prev.libsForQt5.callPackage ./pkg/wrapper.nix {
-        prismlauncher-unwrapped = final.prismlauncher-qt5-unwrapped;
-      };
+    prismlauncher-unwrapped = prev.qt6Packages.callPackage ./pkg unwrappedArgs;
 
-      prismlauncher-unwrapped = prev.qt6Packages.callPackage ./pkg unwrappedArgs;
-
-      prismlauncher = prev.qt6Packages.callPackage ./pkg/wrapper.nix {
-        inherit (final) prismlauncher-unwrapped;
-      };
+    prismlauncher = prev.qt6Packages.callPackage ./pkg/wrapper.nix {
+      inherit (final) prismlauncher-unwrapped;
     };
   };
 }
