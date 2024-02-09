a31a83271f7a0133eb291ed48d6b095541c176a1
初夏同学 <2411829240@qq.com>
2024-02-10 01:41:01 +0800

    Add copyright headers Signed-off-by: 初夏同学 <2411829240@qq.com>

--- launcher/ui/pages/global/InstancesDirListPage.cpp
+++ launcher/ui/pages/global/InstancesDirListPage.cpp
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  Prism Launcher - Minecraft Launcher
+ *  Copyright (c) 2024 初夏同学 <2411829240@qq.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include "InstancesDirListPage.h"
 #include "ui_InstancesDirListPage.h"
