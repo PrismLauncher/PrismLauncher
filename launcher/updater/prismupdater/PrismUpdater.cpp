263b167aaecf1fba215e02484fadc675e4f1f464
Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>
2024-05-16 19:45:00 +0300

    Update launcher/updater/prismupdater/PrismUpdater.cpp
    Signed-off-by: Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>
    (cherry picked from commit 051e7886837090ba62ad81b6f835372649a93b69)

--- launcher/updater/prismupdater/PrismUpdater.cpp
+++ launcher/updater/prismupdater/PrismUpdater.cpp
@@ -474,7 +474,8 @@ PrismUpdaterApp::PrismUpdaterApp(int& argc, char** argv) : QApplication(argc, ar
             target_dir = QDir(m_rootPath).absoluteFilePath("..");
         }
 
-        QMetaObject::invokeMethod(this, [this, target_dir]() { moveAndFinishUpdate(target_dir); }, Qt::QueuedConnection);
+        QMetaObject::invokeMethod(
+           this, [this, target_dir]() { moveAndFinishUpdate(target_dir); }, Qt::QueuedConnection);Qt::QueuedConnection);
 
     } else {
         QMetaObject::invokeMethod(this, &PrismUpdaterApp::loadReleaseList, Qt::QueuedConnection);
