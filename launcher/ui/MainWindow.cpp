e7b4eb99c25d812751b8f30ebdd56e48f8cb2711
Trial97 <alexandru.tripon97@gmail.com>
2024-05-31 18:26:45 +0300

    force fullscreen in gamescope
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/ui/MainWindow.cpp
+++ launcher/ui/MainWindow.cpp
@@ -234,6 +234,8 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWi
         // does not implement decorations
         if (qgetenv("XDG_CURRENT_DESKTOP") == "gamescope") {
             ui->mainToolBar->addAction(ui->actionCloseWindow);
+            showFullScreen();
+            setFixedSize(this->width(), this->height());
         }
 
         ui->actionViewJavaFolder->setEnabled(BuildConfig.JAVA_DOWNLOADER_ENABLED);
