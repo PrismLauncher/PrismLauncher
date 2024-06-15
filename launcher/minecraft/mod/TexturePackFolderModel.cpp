f1d1c9e22c393eafa961b4a3e4f9eb80034c17fb
Trial97 <alexandru.tripon97@gmail.com>
2024-06-15 11:06:20 +0300

    forget sort keys
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/minecraft/mod/TexturePackFolderModel.cpp
+++ launcher/minecraft/mod/TexturePackFolderModel.cpp
@@ -47,7 +47,7 @@ TexturePackFolderModel::TexturePackFolderModel(const QString& dir, BaseInstance*
 {
     m_column_names = QStringList({ "Enable", "Image", "Name", "Last Modified", "Size" });
     m_column_names_translated = QStringList({ tr("Enable"), tr("Image"), tr("Name"), tr("Last Modified"), tr("Size") });
-    m_column_sort_keys = { SortType::ENABLED, SortType::NAME, SortType::NAME, SortType::DATE };
+    m_column_sort_keys = { SortType::ENABLED, SortType::NAME, SortType::NAME, SortType::DATE, SortType::SIZE };
     m_column_resize_modes = { QHeaderView::Interactive, QHeaderView::Interactive, QHeaderView::Stretch, QHeaderView::Interactive,
                               QHeaderView::Interactive };
     m_columnsHideable = { false, true, false, true, true };
