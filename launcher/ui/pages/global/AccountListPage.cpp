357eafd6afde80a42500ad2daeb54c79f946909f
replydev <commoncargo@tutanota.com>
2024-09-19 17:27:12 +0200

    Remove online account check

--- launcher/ui/pages/global/AccountListPage.cpp
+++ launcher/ui/pages/global/AccountListPage.cpp
@@ -141,14 +141,6 @@ void AccountListPage::on_actionAddMicrosoft_triggered()
 
 void AccountListPage::on_actionAddOffline_triggered()
 {
-    if (!m_accounts->anyAccountIsValid()) {
-        QMessageBox::warning(this, tr("Error"),
-                             tr("You must add a Microsoft account that owns Minecraft before you can add an offline account."
-                                "<br><br>"
-                                "If you have lost your account you can contact Microsoft for support."));
-        return;
-    }
-
     MinecraftAccountPtr account =
         OfflineLoginDialog::newAccount(this, tr("Please enter your desired username to add your offline account."));
 
