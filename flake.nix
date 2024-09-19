2663ac5405cbfc3c29a38b32a755b7c7ebda0779
seth <getchoo@tuta.io>
2024-09-19 16:34:59 -0400

    build(nix): add debug builds
    This will lead to build times on par with our other CI jobs, as now
    we're not always building for release
    
    Signed-off-by: seth <getchoo@tuta.io>

--- flake.nix
+++ flake.nix
@@ -118,5 +118,24 @@
         # Only output them if they're available on the current system
         lib.filterAttrs (_: lib.meta.availableOn pkgs.stdenv.hostPlatform) packages
       );
+
+      # We put these under legacyPackages as they are meant for CI, not end user consumption
+      legacyPackages = forAllSystems (
+        system:
+        let
+          prismPackages = self.packages.${system};
+          legacyPackages = self.legacyPackages.${system};
+        in
+        {
+          prismlauncher-debug = prismPackages.prismlauncher.override {
+            prismlauncher-unwrapped = legacyPackages.prismlauncher-unwrapped-debug;
+          };
+
+          prismlauncher-unwrapped-debug = prismPackages.prismlauncher-unwrapped.overrideAttrs {
+            cmakeBuildType = "Debug";
+            dontStrip = true;
+          };
+        }
+      );
     };
 }
