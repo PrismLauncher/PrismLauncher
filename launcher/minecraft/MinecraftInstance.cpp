88a0cf283457f31b22cd2a7f04bf934bdc8c4a40
Jan <sentrycraft123@gmail.com>
2024-10-11 11:27:53 +0200

    Update launcher/minecraft/MinecraftInstance.cpp
    Co-authored-by: Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>
    Signed-off-by: Jan <sentrycraft123@gmail.com>

--- launcher/minecraft/MinecraftInstance.cpp
+++ launcher/minecraft/MinecraftInstance.cpp
@@ -618,34 +618,37 @@ QProcessEnvironment MinecraftInstance::createLaunchEnvironment()
     }
 
     if (settings()->get("UseDiscreteGpu").toBool()) {
+        bool envSet = false;
 #ifdef WITH_QTDBUS
         if (QDBusConnection::systemBus().isConnected()) {
-            QDBusInterface switcheroo("net.hadess.SwitcherooControl", "/net/hadess/SwitcherooControl", "org.freedesktop.DBus.Properties", QDBusConnection::systemBus());
+            QDBusInterface switcheroo("net.hadess.SwitcherooControl", "/net/hadess/SwitcherooControl", "org.freedesktop.DBus.Properties",
+                                      QDBusConnection::systemBus());
 
             if (switcheroo.isValid()) {
-                QDBusReply<QDBusVariant> reply = switcheroo.call(QStringLiteral("Get"), QStringLiteral("net.hadess.SwitcherooControl"), QStringLiteral("GPUs"));
+                QDBusReply<QDBusVariant> reply =
+                    switcheroo.call(QStringLiteral("Get"), QStringLiteral("net.hadess.SwitcherooControl"), QStringLiteral("GPUs"));
                 if (reply.isValid()) {
                     QDBusArgument arg = qvariant_cast<QDBusArgument>(reply.value().variant());
                     QList<QVariantMap> gpus;
                     arg >> gpus;
 
-                    for (const auto &gpu : gpus) {
+                    for (const auto& gpu : gpus) {
                         QString name = qvariant_cast<QString>(gpu[QStringLiteral("Name")]);
                         bool defaultGpu = qvariant_cast<bool>(gpu[QStringLiteral("Default")]);
                         if (!defaultGpu) {
-                            QStringList envList = qvariant_cast<QStringList>(gpu[QStringLiteral("Environment")]);                            
+                            QStringList envList = qvariant_cast<QStringList>(gpu[QStringLiteral("Environment")]);
                             for (int i = 0; i + 1 < envList.size(); i += 2) {
                                 env.insert(envList[i], envList[i + 1]);
                             }
+                            envSet = true;
                             break;
                         }
                     }
                 }
             }
         }
-        else
 #endif
-        {
+        if (!envSet) {
             // Open Source Drivers
             env.insert("DRI_PRIME", "1");
             // Proprietary Nvidia Drivers
@@ -653,6 +656,7 @@ QProcessEnvironment MinecraftInstance::createLaunchEnvironment()
             env.insert("__VK_LAYER_NV_optimus", "NVIDIA_only");
             env.insert("__GLX_VENDOR_LIBRARY_NAME", "nvidia");
         }
+    }
     }
 
     if (settings()->get("UseZink").toBool()) {
