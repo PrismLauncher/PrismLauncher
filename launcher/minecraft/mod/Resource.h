e135750f75e6227f610ad30b4598bca1878efcd9
Trial97 <alexandru.tripon97@gmail.com>
2025-12-12 02:54:14 +0200

    make the update column as a icon
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/minecraft/mod/Resource.h
+++ launcher/minecraft/mod/Resource.h
@@ -125,6 +125,7 @@ class Resource {
     auto provider() const -> QString;
     virtual auto version() const -> QString;
     virtual auto homepage() const -> QString;
+    bool lockUpdate() const;
 
     void setStatus(ResourceStatus status) { m_status = status; }
     void setMetadata(std::shared_ptr<Metadata::ModStruct>&& metadata);
