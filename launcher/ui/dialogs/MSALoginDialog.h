5aad811dd352b4047a5d316afaac57fa42c7d431
Trial97 <alexandru.tripon97@gmail.com>
2026-01-07 02:47:16 +0200

    use qt auth for device code flow
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/ui/dialogs/MSALoginDialog.h
+++ launcher/ui/dialogs/MSALoginDialog.h
@@ -43,7 +43,7 @@ class MSALoginDialog : public QDialog {
     void onDeviceFlowStatus(QString status);
     void onAuthFlowStatus(QString status);
     void authorizeWithBrowser(const QUrl& url);
-    void authorizeWithBrowserWithExtra(QString url, QString code, int expiresIn);
+    void authorizeWithBrowserWithExtra(const QUrl& verificationUrl, const QString& code, const QUrl& completeVerificationUrl);
 
    private:
     Ui::MSALoginDialog* ui;
