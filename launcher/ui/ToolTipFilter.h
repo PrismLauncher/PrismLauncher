54aea98c82994d5d73c87262410fbc0354c12f55
Mark Deneen <mdeneen@gmail.com>
2026-01-09 09:32:33 -0500

    Remove other authors, they should not have been added in the first place
    Signed-off-by: Mark Deneen <mdeneen@gmail.com>

--- launcher/ui/ToolTipFilter.h
+++ launcher/ui/ToolTipFilter.h
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
  *  Prism Launcher - Minecraft Launcher
- *  Copyright (C) 2022 Sefa Eyeoglu <contact@scrumplex.net>
- *  Copyright (C) 2023 TheKodeToad <TheKodeToad@proton.me>
+ *  Copyright (C) 2026 Mark Deneen <mdeneen@gmail.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
