568b30d9147de4f3d9f7ee44293bbe03485f1dce
TheKodeToad <TheKodeToad@proton.me>
2026-02-12 22:18:42 +0000

    Allow -Duser.language to be overriden
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>
    (cherry picked from commit 6bbbfa06d182cc4ff01fb8c1653a9f463d07c0b9)

--- launcher/minecraft/MinecraftInstance.cpp
+++ launcher/minecraft/MinecraftInstance.cpp
@@ -566,6 +566,8 @@ QStringList MinecraftInstance::javaArguments()
 {
     QStringList args;
 
+    args << "-Duser.language=en";
+
     // custom args go first. we want to override them if we have our own here.
     args.append(extraArguments());
 
@@ -618,8 +620,6 @@ QStringList MinecraftInstance::javaArguments()
             args << QString("-XX:PermSize=%1m").arg(permgen);
         }
     }
-
-    args << "-Duser.language=en";
 
     if (javaVersion.isModular() && shouldApplyOnlineFixes())
         // allow reflective access to java.net - required by the skin fix
