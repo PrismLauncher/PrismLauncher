e2952b878d8fdf4eb56f0bd5c6f324c954a00de9
Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>
2026-02-13 08:37:36 +0200

    Revert "Revert "[Backport release-10.x] refactor(console): attach console early""

--- launcher/Application.h
+++ launcher/Application.h
@@ -271,11 +271,6 @@ class Application : public QApplication {
     Qt::ApplicationState m_prevAppState = Qt::ApplicationInactive;
 #endif
 
-#if defined Q_OS_WIN32
-    // used on Windows to attach the standard IO streams
-    bool consoleAttached = false;
-#endif
-
     // FIXME: attach to instances instead.
     struct InstanceXtras {
         InstanceWindow* window = nullptr;
