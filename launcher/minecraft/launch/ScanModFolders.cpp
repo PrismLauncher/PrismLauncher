b0cba468d9ed5502316ee230a3d0350d5f9f6699
dyredhead <danielyentin@gmail.com>
2026-05-31 17:01:03 -0400

    added a bunch of disconnect safeguards to ScanModFolders to avoid triggering ASSERT_NEVER(!isRunning()) in Task.cpp
    Signed-off-by: dyredhead <danielyentin@gmail.com>

--- launcher/minecraft/launch/ScanModFolders.cpp
+++ launcher/minecraft/launch/ScanModFolders.cpp
@@ -66,25 +66,63 @@ void ScanModFolders::executeTask()
 
 void ScanModFolders::modsDone()
 {
+    qDebug() << "Check done in ScanModFolders modsDone...";
+    if (!isRunning()) {
+        qDebug() << "ScanModFolders::modsDone called but step not running; ignoring.";
+        return;
+    }
     m_modsDone = true;
     checkDone();
 }
 
 void ScanModFolders::coreModsDone()
 {
+    qDebug() << "Check done in ScanModFolders coreModsDone...";
+    if (!isRunning()) {
+        qDebug() << "ScanModFolders::coreModsDone called but step not running; "
+                    "ignoring.";
+        return;
+    }
     m_coreModsDone = true;
     checkDone();
 }
 
 void ScanModFolders::nilModsDone()
 {
+    qDebug() << "Check done in ScanModFolders nilModsDone...";
+    if (!isRunning()) {
+        qDebug() << "ScanModFolders::nilModsDone called but step not running; ignoring.";
+        return;
+    }
     m_nilModsDone = true;
     checkDone();
 }
 
 void ScanModFolders::checkDone()
 {
+    qDebug() << "Check done in ScanModFolders...";
     if (m_modsDone && m_coreModsDone && m_nilModsDone) {
+        // Disconnect model signals before finishing so the finished step doesn't
+        // receive later updateFinished() notifications and try to finish again.
+        auto m_inst = m_parent->instance();
+        if (m_inst) {
+            disconnect(m_inst->loaderModList(), &ModFolderModel::updateFinished, this, &ScanModFolders::modsDone);
+            disconnect(m_inst->coreModList(), &ModFolderModel::updateFinished, this, &ScanModFolders::coreModsDone);
+            disconnect(m_inst->nilModList(), &ModFolderModel::updateFinished, this, &ScanModFolders::nilModsDone);
+        }
+
         emitSucceeded();
     }
+}
+
+void ScanModFolders::finalize()
+{
+    auto m_inst = m_parent->instance();
+    if (!m_inst) {
+        return;
+    }
+
+    disconnect(m_inst->loaderModList(), &ModFolderModel::updateFinished, this, &ScanModFolders::modsDone);
+    disconnect(m_inst->coreModList(), &ModFolderModel::updateFinished, this, &ScanModFolders::coreModsDone);
+    disconnect(m_inst->nilModList(), &ModFolderModel::updateFinished, this, &ScanModFolders::nilModsDone);
 }
--- launcher/minecraft/launch/ScanModFolders.h
+++ launcher/minecraft/launch/ScanModFolders.h
@@ -22,10 +22,12 @@ class ScanModFolders : public LaunchStep {
     Q_OBJECT
    public:
     explicit ScanModFolders(LaunchTask* parent) : LaunchStep(parent) {};
-    virtual ~ScanModFolders() {};
+    virtual ~ScanModFolders() = default;
 
     virtual void executeTask() override;
     virtual bool canAbort() const override { return false; }
+    virtual void finalize() override;
+
    private slots:
     void coreModsDone();
     void modsDone();
