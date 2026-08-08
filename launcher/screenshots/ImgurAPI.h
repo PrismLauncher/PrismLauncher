06bef9425d29ab08a9fac382f1d0b2a833da118c
Trial97 <alexandru.tripon97@gmail.com>
2026-08-08 11:21:48 +0300

    refactor(screenshots): merge Imgur into ImgurAPI
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/screenshots/ImgurAPI.h
+++ launcher/screenshots/ImgurAPI.h
@@ -2,6 +2,7 @@
 /*
  *  Prism Launcher - Minecraft Launcher
  *  Copyright (c) 2022 flowln <flowlnlnln@gmail.com>
+ *  Copyright (C) 2022 Sefa Eyeoglu <contact@scrumplex.net>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -35,38 +36,20 @@
 
 #pragma once
 
+#include <QString>
+#include <utility>
+
 #include "Screenshot.h"
 #include "net/NetRequest.h"
 
-class ImgurAlbumCreation : public Net::NetRequest {
-   public:
-    virtual ~ImgurAlbumCreation() = default;
+namespace ImgurAPI {
 
-    struct Result {
-        QString deleteHash;
-        QString id;
-    };
-
-    class Sink : public Net::Sink {
-       public:
-        Sink(std::shared_ptr<Result> res) : m_result(res) {};
-        virtual ~Sink() = default;
-
-       public:
-        auto init(QNetworkRequest& request) -> Task::State override;
-        auto write(QByteArray& data) -> Task::State override;
-        auto abort() -> Task::State override;
-        auto finalize(QNetworkReply& reply) -> Task::State override;
-        auto hasLocalData() -> bool override { return false; }
-
-       private:
-        std::shared_ptr<Result> m_result;
-        QByteArray m_output;
-    };
-
-    static NetRequest::Ptr make(std::shared_ptr<Result> output, QList<ScreenShot::Ptr> screenshots);
-    QNetworkReply* getReply(QNetworkRequest& request) override;
-
-   private:
-    QList<ScreenShot::Ptr> m_screenshots;
+struct AlbumResult {
+    QString deleteHash;
+    QString id;
 };
+
+std::pair<Net::NetRequest::Ptr, QString*> makeUpload(ScreenShot::Ptr shot);
+std::pair<Net::NetRequest::Ptr, AlbumResult*> makeAlbum(const QList<ScreenShot::Ptr>& screenshots);
+
+}  // namespace ImgurAPI
