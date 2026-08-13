3680a052d0359d4e02668153ca3ac4ed9196fc12
Trial97 <alexandru.tripon97@gmail.com>
2026-08-13 10:08:34 +0300

    style: modernize net sinks and skin dialog code
    Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

--- launcher/net/DummySink.h
+++ launcher/net/DummySink.h
@@ -23,12 +23,12 @@ namespace Net {
 
 class DummySink : public Sink {
    public:
-    explicit DummySink() {}
-    ~DummySink() override {}
-    auto init(QNetworkRequest& request) -> Task::State override { return Task::State::Running; }
-    auto write(QByteArray& data) -> Task::State override { return Task::State::Succeeded; }
+    explicit DummySink() = default;
+    ~DummySink() override = default;
+    auto init(QNetworkRequest& /*request*/) -> Task::State override { return Task::State::Running; }
+    auto write(QByteArray& /*data*/) -> Task::State override { return Task::State::Succeeded; }
     auto abort() -> Task::State override { return Task::State::AbortedByUser; }
-    auto finalize(QNetworkReply& reply) -> Task::State override { return Task::State::Succeeded; }
+    auto finalize(QNetworkReply& /*reply*/) -> Task::State override { return Task::State::Succeeded; }
     auto hasLocalData() -> bool override { return false; }
 };
 
