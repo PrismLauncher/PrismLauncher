790d35eafb09e7105c2562f02ee25ed674c84a25
cecilia <alcremieow@posteo.de>
2026-08-30 14:57:30 +0200

    Add option for template folder that gets copied to newly created instances
    Signed-off-by: cecilia <alcremieow@posteo.de>

--- launcher/InstanceList.cpp
+++ launcher/InstanceList.cpp
@@ -39,6 +39,7 @@
 #include <QDebug>
 #include <QDirListing>
 #include <QFile>
+#include <QDir>
 #include <QFileInfo>
 #include <QJsonArray>
 #include <QJsonDocument>
@@ -1138,6 +1139,13 @@ bool InstanceList::commitStagedInstance(const QString& path, const InstanceTask&
         }
 
         m_instanceSet.insert(instID);
+
+        QString templateDir = APPLICATION->settings()->get("TemplateDir").toString();
+        if (!templateDir.isEmpty() && QDir(templateDir).exists()){
+            if (!FS::copy(templateDir, destination)()) {
+                qWarning() << "Failed to copy instance template";
+            }
+        }
 
         emit instancesChanged();
         emit instanceSelectRequest(instID);
