X-Pro72/DabMC

make the update column as a icon
Signed-off-by: Trial97 <alexandru.tripon97@gmail.com>

launcher/minecraft/mod/TexturePackFolderModel.cpp
@@ -82,9 +82,11 @@ QVariant TexturePackFolderModel::data(const QModelIndex& index, int role) const
         case Qt::CheckStateRole:
             if (column == ActiveColumn) {
                 return m_resources[row]->enabled() ? Qt::Checked : Qt::Unchecked;
-            } else if (column == LockUpdateCoumn) {
-                return !m_resources[row]->lockUpdate() ? Qt::Checked : Qt::Unchecked;
             }
+            return {};
+        case Qt::UserRole:
+            if (column == LockUpdateCoumn)
+                return at(row).lockUpdate();
             return {};
         default:
             break;

PrismLauncher/PrismLauncher

launcher/minecraft/mod/TexturePackFolderModel.cpp
@@ -86,9 +86,11 @@ QVariant TexturePackFolderModel::data(const QModelIndex& index, int role) const
         case Qt::CheckStateRole:
             if (column == ActiveColumn) {
                 return m_resources[row]->enabled() ? Qt::Checked : Qt::Unchecked;
-            } else if (column == LockUpdateColumn) {
-                return !m_resources[row]->lockUpdate() ? Qt::Checked : Qt::Unchecked;
             }
+            return {};
+        case Qt::UserRole:
+            if (column == LockUpdateColumn)
+                return at(row).lockUpdate();
             return {};
         default:
             break;