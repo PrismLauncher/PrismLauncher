1b404bf24732b6c687111349dffa4c32bcd4cd7d
Yundi Xu <yundi.eric.xu@gmail.com>
2026-09-05 10:45:53 -0500

    fix shortcut world launch
    * World list now correctly loads asynchronously
    * --world arg now correctly points to directory name
    
    AI Dislcaimer: Claude Sonnet 5 used for code generation. All generated
    code have been manually reviewed by Yundi Xu.
    
    Signed-off-by: Yundi Xu <yundi.eric.xu@gmail.com>

--- launcher/ui/dialogs/CreateShortcutDialog.h
+++ launcher/ui/dialogs/CreateShortcutDialog.h
@@ -18,6 +18,8 @@
 #include <QDialog>
 
 class MinecraftInstance;
+class WorldList;
+class QSortFilterProxyModel;
 
 namespace Ui {
 class CreateShortcutDialog;
@@ -51,8 +53,11 @@ class CreateShortcutDialog : public QDialog {
     Ui::CreateShortcutDialog* ui;
     QString InstIconKey;
     MinecraftInstance* m_instance;
-    bool m_QuickJoinSupported = false;
+    bool m_quickJoinSupported = false;
+    WorldList* m_worldList = nullptr;
+    QSortFilterProxyModel* m_worldProxyModel = nullptr;
 
     // Functions
     void stateChanged();
+    void updateWorldTargetVisibility();
 };
