2e26d80af2268a5f45a8711ebf26ecf441e48ce6
TheKodeToad <TheKodeToad@proton.me>
2026-09-10 21:44:41 +0100

    Remove unused SettingChanged slot
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

--- launcher/icons/IconList.h
+++ launcher/icons/IconList.h
@@ -100,7 +100,6 @@ class IconList : public QAbstractListModel {
 
    protected slots:
     void fileChanged(const QString& path);
-    void SettingChanged(const Setting& setting, const QVariant& value);
 
    private:
     shared_qobject_ptr<QFileSystemWatcher> m_watcher;
