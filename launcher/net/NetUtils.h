88cc8cd9fc0b1792f9d04eb3bb6e46095b65c775
bunchy7s <bunchy7s@tuta.io>
2026-09-20 19:46:26 -0500

    add 403 (ContentAccessDenied) offline mode fallback to NetUtils.h
    Signed-off-by: bunchy7s <bunchy7s@bunchy7s.dev>

--- launcher/net/NetUtils.h
+++ launcher/net/NetUtils.h
@@ -47,6 +47,7 @@ inline bool isApplicationError(QNetworkReply::NetworkError x)
 inline bool isServerError(QNetworkReply::NetworkError x)
 {
     static QSet<QNetworkReply::NetworkError> errors = { QNetworkReply::InternalServerError,
+                                                        QNetworkReply::ContentAccessDenied,         // 403 | seen in logs in 2026
                                                         QNetworkReply::OperationNotImplementedError,
                                                         QNetworkReply::ServiceUnavailableError,     // 503 | seen in logs in 2026
                                                         //QNetworkReply::GatewayTimeoutError,       // 504 | seen in logs in 2024
