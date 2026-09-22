544be7686b67e7091f03fc537ef10ec31e05e5b2
TheKodeToad <TheKodeToad@proton.me>
2026-09-22 13:16:36 +0100

    Remove unnecessary duplicate code for empty release type fallback
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

--- launcher/ui/pages/instance/ModFolderPage.cpp
+++ launcher/ui/pages/instance/ModFolderPage.cpp
@@ -261,13 +261,7 @@ void ModFolderPage::updateMods(bool includeDeps, std::vector<ModPlatform::Indexe
         modsList = m_model->allResources();
     }
 
-    if (releaseTypes.empty()) {
-        auto settingVal =
-            m_instance ? m_instance->settings()->get("ModUpdateReleaseTypes") : APPLICATION->settings()->get("ModUpdateReleaseTypes");
-        releaseTypes = ModPlatform::IndexedVersionType::fromStringList(Json::toStringList(settingVal.toString()));
-    }
-
-    ResourceUpdateDialog updateDialog(this, m_instance, m_model, modsList, includeDeps, profile->getModLoadersList(), releaseTypes);
+    ResourceUpdateDialog updateDialog(this, m_instance, m_model, modsList, includeDeps, profile->getModLoadersList(), std::move(releaseTypes));
     updateDialog.checkCandidates();
 
     if (updateDialog.aborted()) {
