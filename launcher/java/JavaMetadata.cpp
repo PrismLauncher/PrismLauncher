d81d4737105a84d5a211c643cd399678675179cb
Vishrut Sachan <vishrutsachan2004@gmail.com>
2026-09-24 14:25:58 +0530

    Read Java meta version fields without requiring them
    Signed-off-by: Vishrut Sachan <vishrutsachan2004@gmail.com>

--- launcher/java/JavaMetadata.cpp
+++ launcher/java/JavaMetadata.cpp
@@ -68,11 +68,11 @@ Result<MetadataPtr> parseJavaMeta(const QJsonObject& in)
 
     if (in.contains("version")) {
         TRY_INTO(const auto& version, Json::requireObject(in, "version"))
-        TRY_INTO(const auto& name, version.contains("name") ? Json::requireString(version, "name") : Result<QString>{})
-        TRY_INTO(const auto& major, Json::requireInteger(version, "major"))
-        TRY_INTO(const auto& minor, Json::requireInteger(version, "minor"))
-        TRY_INTO(const auto& security, Json::requireInteger(version, "security"))
-        TRY_INTO(const auto& build, version.contains("build") ? Json::requireInteger(version, "build") : Result<int>{ 0 })
+        const auto& name = version["name"].toString("");
+        const auto& major = version["major"].toInteger();
+        const auto& minor = version["minor"].toInteger();
+        const auto& security = version["security"].toInteger();
+        const auto& build = version["build"].toInteger();
         meta->version = JavaVersion(major, minor, security, build, name);
     }
     return meta;
